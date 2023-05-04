c33c9b2b3efda4d9df82e4d2809e18423f79f788
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-04 13:32:36 +0530

    Extension Method String

---
+++ ExtensionMethod1/ExtensionMethod1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first word:");
+            string s =Console.ReadLine();
+            Console.WriteLine("Enter second word:");
+            string snew = Console.ReadLine();
+            string result=s.Concatenate(snew);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ExtensionMethod1/ExtensionMethod1/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod1
+{
+    public static class StringExtension
+    {
+        public static string Concatenate(this string s, string snew)
+        { 
+            return s+snew;
+        }
+    }
+}
